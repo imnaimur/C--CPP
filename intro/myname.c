71b7139710cb6efab5c241f7446a5de36a71a314
Arman <naimur58co@gmail.com>
2021-07-24 18:24:56 +0600

    Formating

--- intro/myname.c
+++ intro/myname.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 int main()
 {
-printf("my name is Arman\n");
-printf("i am learning c programming\n");
-printf("%d\n",5+5);
-printf("%d\n",5-5);
-printf("%d\n",5*5);
-printf("%d\n",5/5);
-
-
-return 0;
-
-
+    printf("my name is Arman\n");
+    printf("i am learning c programming\n");
+    printf("%d\n", 5 + 5);
+    printf("%d\n", 5 - 5);
+    printf("%d\n", 5 * 5);
+    printf("%d\n", 5 / 5);
+    return 0;
 }
--- intro/op.c
+++ intro/op.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main()
 {
-int a=5;
-float b=3.2f;
-float c = a + b;
-printf("%d+%f=%f\n",a,b,a+b);
-printf("%d-%f=%f\n",a,b,a-b);
-printf("%d*%f=%f\n",a,b,a*b);
-printf("%d/%f=%f\n",a,b,a/b);
-printf("a=%d b=%f\n",a,b);
-return 0;
+    int a = 5;
+    float b = 3.2f;
+    float c = a + b;
+    printf("%d+%f=%f\n", a, b, a + b);
+    printf("%d-%f=%f\n", a, b, a - b);
+    printf("%d*%f=%f\n", a, b, a * b);
+    printf("%d/%f=%f\n", a, b, a / b);
+    printf("a=%d b=%f\n", a, b);
+    return 0;
 }
