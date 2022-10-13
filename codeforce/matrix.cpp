b554c4f4ab1d697e2ba7a92800a71bb051bb3dce
Naimur <naimur58co@gmail.com>
2022-10-14 00:22:53 +0600

    codeforce

--- codeforce/matrix.cpp
+++ codeforce/matrix.cpp
@@ -2,8 +2,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int move;
+    int move = 0,i,j,x;//i = row j = column
+    for(i = 1;i<=5;i++){
+        for(j = 1;j<=5;j++){
+            cin >> x;
+            if(x == 1){
+                cout<< abs(i-3)+abs(j-3)<< endl;
+            }
+        }
+    }
 }
+
 /*
 0 0 0 0 0
 0 0 0 0 1
