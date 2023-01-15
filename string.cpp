433151fa19b1601a49952dc002f7b3c2a05752cc
Naimur <naimur58co@gmail.com>
2023-01-15 19:09:09 +0600

    string problem icpc assuis grp

---
+++ string.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    string s1,s2;
+    cin >> s1 >> s2;
+    int n1,n2;
+    n1 = s1.length();
+    n2 = s2.length();
+    cout<< n1 << " "<< n2 <<endl << s1 << " " << s2 << endl; 
+}
