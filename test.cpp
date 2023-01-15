ea9bd3f5f6ecfd4d001914249f3dd34788bac06b
Naimur <naimur58co@gmail.com>
2023-01-16 00:32:13 +0600

    vector + structure

--- test.cpp
+++ test.cpp
@@ -3,14 +3,33 @@
 #include<vector>
 using namespace std;
 
+// int main(){
+//     vector<int> v;
+//     int n;
+//     cin >> n;
+//     for(int i = 0; i< n; i++){
+//         int a;
+//         cin >> a;
+//         v.push_back(a);
+//     }
+//     cout << v[1];
+// }
+
 int main(){
-    vector<int> v;
-    int n;
-    cin >> n;
-    for(int i = 0; i< n; i++){
-        int a;
-        cin >> a;
-        v.push_back(a);
+    struct
+    {
+        vector<int> id;
+        vector<string> name;
+    }student;
+    for(int i = 0; i< 3; i++){
+        int n;
+        cout<< "enter id :";
+        cin>> n;
+        cout<< "Enter name: ";
+        string nm;
+        cin >> nm;
+        student.id.push_back(n);
+        student.name.push_back(nm);
     }
-    cout << v[1];
+    cout << student.id[1];
 }
